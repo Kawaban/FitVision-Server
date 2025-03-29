97009502b6f5697b795a330adb8feb861be04efe
AliakseiSamoshyn(272 <aliakseisamoshyn@gmail.com>
2025-03-29 23:19:01 +0100

    Init

---
+++ src/main/java/org/example/fitvision/FitVisionApplication.java
@@ -0,0 +1,13 @@
+package org.example.fitvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitVisionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitVisionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fitvision/FitVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fitvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitVisionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
